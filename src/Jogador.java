bcde0629ebc4a720f0b8609a9ae6a69eb5bd2859
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-09-23 22:39:35 -0300

    Feat: Criar classe para gravar as informações dos jogadores,e Criar método para retornar o simbolo X ou O

--- src/Jogador.java
+++ src/Jogador.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Jogador {
+public class Jogador {
+    private char Velha;
+
+    public Jogador(char Velha) {
+        this.Velha = Velha;
+    }
+
+    public char getVelha() {
+        return Velha;
+    }
+
+    public static Jogador alternarJogador(Jogador jogadorAtual) {
+        if(jogadorAtual.getVelha() == 'X') {
+
+        }
+    }
 }
