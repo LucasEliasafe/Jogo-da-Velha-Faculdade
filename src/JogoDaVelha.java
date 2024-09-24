37e73fb8df84d4870004745ce7af06533a034a2a
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-09-23 22:49:47 -0300

    Feat: Criar uma Classe Jogo da Velha, adicionar as variaveis do tabuleiro e jogador, Criar método para o menu, e criar um While para as informações do Menu

--- src/JogoDaVelha.java
+++ src/JogoDaVelha.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class JogoDaVelha {
+import java.util.Scanner;
+
+public class JogoDaVelha {
+    private Tabuleiro tabuleiro;
+    private Jogador jogadorAtual;
+
+    public JogoDaVelha() {
+        this.tabuleiro = new Tabuleiro();
+        this.jogadorAtual = new Jogador('X');
+    }
+
+    public void mostrarMenu() {
+        Scanner scanner = new Scanner(System.in);
+        boolean continuar = true;
+
+
+        while (continuar) {
+            System.out.println("\nMENU do Jogo da Velha\n");
+            System.out.println("1 - Iniciar novo Jogo\n");
+            System.out.println("2 - Sair\n");
+            System.out.print("Escolha uma opção: ");
+            int opcao = scanner.nextInt();
+        }
+    }
 }
