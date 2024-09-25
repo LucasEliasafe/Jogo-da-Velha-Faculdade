ba50250ae07d5754bee1f903f6a303b2b9cbde56
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-09-24 22:21:20 -0300

    Feat: Adicionar uma switch case e arrumar o menu para poder retornar e não perder os dados do jogo

--- src/JogoDaVelha.java
+++ src/JogoDaVelha.java
@@ -3,10 +3,12 @@ import java.util.Scanner;
 public class JogoDaVelha {
     private Tabuleiro tabuleiro;
     private Jogador jogadorAtual;
+    private boolean jogoEmAndamento;
 
     public JogoDaVelha() {
         this.tabuleiro = new Tabuleiro();
         this.jogadorAtual = new Jogador('X');
+        this.jogoEmAndamento = false;
     }
 
     public void mostrarMenu() {
@@ -17,7 +19,10 @@ public class JogoDaVelha {
         while (continuar) {
             System.out.println("\nMENU do Jogo da Velha\n");
             System.out.println("1 - Iniciar novo Jogo\n");
-            System.out.println("2 - Sair\n");
+            if (jogoEmAndamento) {
+                System.out.println("2 - Continuar Jogo Atual");
+            }
+            System.out.println("3 - Sair");
             System.out.print("Escolha uma opção: ");
             int opcao = scanner.nextInt();
 
@@ -26,6 +31,13 @@ public class JogoDaVelha {
                     iniciar();
                     break;
                 case 2:
+                    if (jogoEmAndamento) {
+                        continuarJogo();
+                    } else {
+                        System.out.println("Nenhum jogo em andamento. Escolha 'Iniciar novo Jogo'.");
+                    }
+                    break;
+                case 3:
                     continuar = false;
                     System.out.println("Saindo do jogo...");
                     break;
@@ -37,23 +49,31 @@ public class JogoDaVelha {
         scanner.close();
     }
 
+
     public void iniciar() {
         tabuleiro.reiniciar();
         jogadorAtual = new Jogador('X');
-        boolean jogoEmAndamento = true;
+        jogoEmAndamento = true;
+        continuarJogo();
+    }
+
+
+    public void continuarJogo() {
         Scanner scanner = new Scanner(System.in);
 
         while (jogoEmAndamento) {
             tabuleiro.exibir();
-            System.out.println("Jogador " + jogadorAtual.getVelha() + ", escolha um número (1-9): ");
-            System.out.println("Digite 1-9 para fazer uma jogada");
-            System.out.println("Digite 0 para voltar a última jogada");
+            System.out.println("Jogador " + jogadorAtual.getVelha() + ", escolha uma opção: ");
+            System.out.println("1-9 para fazer uma jogada, 0 para voltar a última jogada, 99 para sair ao menu");
             int jogada = scanner.nextInt();
 
-            if (jogada == 0) {
+            if (jogada == 99) {
+                System.out.println("Voltando ao menu...");
+                return; // Volta para o menu, sem perder o estado do jogo
+            } else if (jogada == 0) {
                 if (tabuleiro.desfazerUltimaJogada()) {
                     System.out.println("Última jogada desfeita.");
-                    jogadorAtual = Jogador.alternarJogador(jogadorAtual);
+                    jogadorAtual = Jogador.alternarJogador(jogadorAtual); // Alterna o jogador de volta
                 } else {
                     System.out.println("Não há jogadas para desfazer.");
                 }
@@ -62,19 +82,20 @@ public class JogoDaVelha {
                 if (tabuleiro.verificar()) {
                     tabuleiro.exibir();
                     System.out.println("Jogador " + jogadorAtual.getVelha() + " venceu!");
-                    jogoEmAndamento = false;
+                    jogoEmAndamento = false; // O jogo termina após vitória
                 } else if (tabuleiro.tabuleirocompleto()) {
                     tabuleiro.exibir();
                     System.out.println("Empate!");
-                    jogoEmAndamento = false;
+                    jogoEmAndamento = false; // O jogo termina em empate
                 } else {
-                    jogadorAtual = Jogador.alternarJogador(jogadorAtual);
+                    jogadorAtual = Jogador.alternarJogador(jogadorAtual); // Alterna o jogador
                 }
             } else {
                 System.out.println("Jogada inválida, tente novamente.");
             }
         }
     }
+
 
 
     public static void main(String[] args) {
