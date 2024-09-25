aeab40ef1e80ed25f7fe4b3fde701e19a054748b
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-09-24 21:19:51 -0300

    Feat: Criar switch para as definições do menu e colocar um iniciar, depois criar um método para iniciar o jogo

--- src/JogoDaVelha.java
+++ src/JogoDaVelha.java
@@ -20,6 +20,51 @@ public class JogoDaVelha {
             System.out.println("2 - Sair\n");
             System.out.print("Escolha uma opção: ");
             int opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    iniciar();
+                    break;
+                case 2:
+                    continuar = false;
+                    System.out.println("Saindo do jogo...");
+                    break;
+                default:
+                    System.out.println("Opção inválida! Tente novamente.");
+                    break;
+            }
+        }
+        scanner.close();
+    }
+
+    public void iniciar() {
+        boolean jogoEmAndamento = true;
+        Scanner scanner = new Scanner(System.in);
+
+        while (jogoEmAndamento) {
+            tabuleiro.exibir();
+            System.out.println("Jogador " + jogadorAtual.getVelha() + ", escolha um número (1-9): ");
+            int jogada = scanner.nextInt();
+
+            if (tabuleiro.jogadaPermitida(jogada)) {
+                tabuleiro.realizar(jogada, jogadorAtual.getVelha());
+                if (tabuleiro.verificar()) {
+                    tabuleiro.exibir();
+                    System.out.println("Jogador " + jogadorAtual.getVelha() + " venceu!");
+                    jogoEmAndamento = false;
+                } else if (tabuleiro.tabuleirocompleto()) {
+                    tabuleiro.exibir();
+                    System.out.println("Empate!");
+                    jogoEmAndamento = false;
+                } else {
+                    jogadorAtual = Jogador.alternarJogador(jogadorAtual);
+                }
+            } else {
+                System.out.println("Jogada inválida, tente novamente.");
+            }
         }
     }
+
+
+
 }
